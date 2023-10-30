ce84dfb646c6999134b4faa06937c22746b95a88
MathieuD-QA <mathieu.dassie@pm.me>
2023-10-30 14:15:44 +0100

    feat: add endpoint get-testcase

--- app/Controllers/Http/TestcasesController.ts
+++ app/Controllers/Http/TestcasesController.ts
@@ -3,11 +3,30 @@ import TestcaseValidator from '../../Validators/TestcaseValidator'
 import Testcase from '../../Models/Testcase'
 
 export default class TestcasesController {
+
+
+
+  public async index({ response }: HttpContextContract) {
+    try {
+      const res = await Testcase.all()
+      return response.status(200).send(res)
+    } catch (error) {
+      return response.status(400).send('error')
+    }
+  }
+  public async show({ response, params }: HttpContextContract) {
+    try {
+      const id = await Testcase.findOrFail(params.id)
+      return response.status(200).send(id)
+    } catch (error) {
+      return response.status(400).send({message: 'bad id' })
+    }
+  }
   public async create({ request, response }: HttpContextContract) {
     try {
       const payload = await request.validate(TestcaseValidator)
       await Testcase.create(payload)
-      return response.status(200).send({ message: 'Données insérées avec succès' })
+      return response.status(201).send({ message: 'Données insérées avec succès' })
     } catch (error) {
       console.error(error.messages)
       return response.status(400).send(error.messages)
